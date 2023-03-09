74870d43a0e9e7f524c2fbd781f857f332697115
Prathamesh Madniwale <61145586+prathamesh2525@users.noreply.github.com>
2023-03-09 21:31:41 +0530

    updated responsive code

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,13 +12,15 @@ export const Home = () => {
   }, [])
   return (
     <div className="gradient-two">
-      <h1 className="text-4xl font-bold pt-8 text-white">Meme Generator</h1>
-      <div className="flex justify-center items-center relative">
-        <div className="fixed bottom-5 left-4 px-4 py-2 text-2xl bg-blue-500 transition duration-200  hover:bg-blue-700 font-bold text-white rounded-xl">
-          <Link to={"/"}>◄</Link>
-        </div>
-        <div className="grid md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-5 gap-4 p-8">
-          {memes && memes.map((meme) => <Card meme={meme} key={meme.id} />)}
+      <div className="w-[80%]">
+        <h1 className="text-4xl font-bold pt-8 text-white">Meme Generator</h1>
+        <div className="flex justify-center items-center relative">
+          <div className="fixed bottom-5 left-4 px-4 py-2 text-2xl bg-blue-500 transition duration-200  hover:bg-blue-700 font-bold text-white rounded-xl">
+            <Link to={"/"}>◄</Link>
+          </div>
+          <div className="grid md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-5 gap-4 p-8">
+            {memes && memes.map((meme) => <Card meme={meme} key={meme.id} />)}
+          </div>
         </div>
       </div>
     </div>
